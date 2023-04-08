c08d4236cd5809dac345095ac548e5f8d9987f10
raymoon-code <raymoonedu@gmail.com>
2023-04-07 19:20:10 -0500

    update

--- index.py
+++ index.py
@@ -1,11 +1,1 @@
-from website import create_app
-# from website import create_database
-# from website import db
-
-app = create_app()
-
-if __name__ == '__main__':
-    # with app.app_context():
-    #     db.create_all()
-    #     print('Database created')
-    app.run(debug=True)
+from wsgi import app
