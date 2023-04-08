fbf8272b79a9bf3d4ecd334132fa2936bc3136bf
raymoon-code <raymoonedu@gmail.com>
2023-04-07 21:47:30 -0500

    main, require

---
+++ main.py
@@ -0,0 +1,11 @@
+from website import create_app
+# from website import create_database
+# from website import db
+
+app = create_app()
+
+if __name__ == '__main__':
+    # with app.app_context():
+    #     db.create_all()
+    #     print('Database created')
+    app.run(debug=True, host="0.0.0.0", PORT=5000)
