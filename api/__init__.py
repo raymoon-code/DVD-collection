172418936b24986b6b1d9323d5b73d66091c4f7e
raymoon-code <raymoonedu@gmail.com>
2023-04-08 12:12:02 -0500

    edit init

--- api/__init__.py
+++ api/__init__.py
@@ -11,6 +11,7 @@ def create_app():
     
     app.config['SECRET_KEY'] = 'RAYMOON'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import views
